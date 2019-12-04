1230ffab2ba453573fe5c642200e95abc8ab2bae
Javier Goncalves <jagi.ccs@gmail.com>
2019-12-04 12:54:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(string)
-  result = ""
-  string.each {|x| result = x.reverse}
-  result
+  # result = ""
+  # string.each {|x| result = x.reverse}
+  # result
+  string.split.reverse.join("")
 end
