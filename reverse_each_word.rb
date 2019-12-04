05598825ee6dc41ae14d915b862b492e16522bf6
Javier Goncalves <jagi.ccs@gmail.com>
2019-12-04 13:05:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
   array = string.split(" ")
   last_message = []
-  array.collect do |string|
-    array << string.reverse
+  array.collect do |words|
+    array << words.reverse
   end
   array.join(" ")
 end
